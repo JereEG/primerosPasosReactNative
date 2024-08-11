b1ee91477178a0965e19c766349e4d0f15cabf75
JereEG <jeremiasgonzalez7464@gmail.com>
2024-08-11 00:22:12 -0300

    SIguiendo documentacion oficial

--- Navigation.js
+++ Navigation.js
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+//importar screens
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
+import StackScreen from './screens/StackScreen';
 
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
+      <Tab.Navigator 
+        initialRouteName="Home"
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
